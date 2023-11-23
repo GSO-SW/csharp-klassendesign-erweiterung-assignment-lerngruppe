e3c02e9934fa1dbe596a07c7d0d34e5dbc8e9b81
Kander Akinci <87019962+AI-Assistant@users.noreply.github.com>
2023-11-23 18:02:24 +0000

    UD

--- Aufgabe1_Ueberweisung/Program.cs
+++ Aufgabe1_Ueberweisung/Program.cs
@@ -19,7 +19,7 @@ public class Bank
 {
     private List<Bankkonto> konto_liste = new List<Bankkonto>();
     private Bankkonto aktives_konto = new Bankkonto();
-    private double gebuehren = 0.01;
+
     public void MenueAufruf()
     {
         while (true)
@@ -30,7 +30,7 @@ public class Bank
             }
 
             Console.WriteLine($"Ihre Kontonummer: {aktives_konto.kontonummer}");
-            Console.WriteLine("\nWählen Sie eine Option:\n1. Konto erstellen\n2. Konto auswählen\n3. Einzahlen\n4. Abheben\n5. Kontostand anzeigen\n6. Überweisen\n7. Beenden");
+            Console.WriteLine("\nWählen Sie eine Option:\n1. Konto erstellen\n2. Konto auswählen\n3. Einzahlen\n4. Abheben\n5. Kontostand anzeigen\n6.  Beenden");
             string option = Console.ReadLine();
 
             if ((option == "3" || option == "4" || option == "5") && aktives_konto.kontonummer == 0)
@@ -83,9 +83,6 @@ public class Bank
                         Console.ReadLine();
                         break;
                     case "6":
-                        Ueberweisung();
-                        break;
-                    case "7":
                         Console.WriteLine("Auf Wiedersehen!");
                         Console.ReadLine();
                         return;
@@ -101,60 +98,12 @@ public class Bank
     }
 
     //Erstellen Sie hier eine Überweisungsmehtode
-    private void Ueberweisung()
-    {
-        Bankkonto empfaenger =null;
-        bool flag;
 
-        do
-        {
-            flag= false;    
-            Console.Clear();
 
-            Console.WriteLine("Geben Sie die Kontonummer des Zahlungsenpfängers an:");
-            int empfaenger_kn = Convert.ToInt32(Console.ReadLine());
-
-            foreach (Bankkonto bankkonto in konto_liste)
-            {
-                if (bankkonto.kontonummer == empfaenger_kn)
-                {
-                    empfaenger = bankkonto;
-                }
-            }
-            if (empfaenger == null)
-            {
-                Console.WriteLine("Konto nicht gefunden");
-                Console.ReadLine();
-
-                Console.WriteLine("Erneut Versuchen(Y/N)?");
-                string eingabe = Console.ReadLine();
-                if (eingabe == "Y")
-                {
-                    flag = true;
-                }
-            }
-
-            else
-            {
-                Console.WriteLine("Geben Sie die den Überweisungsbetrag an:");
-                double betrag = Convert.ToDouble(Console.ReadLine());
-
-                double betrag_gesamt = betrag + (betrag * gebuehren);
-
-                aktives_konto.Auszahlung(betrag_gesamt);
-                empfaenger.Einzahlung(betrag);
-
-                Console.WriteLine("Überweisung Erfolgreich");
-                Console.ReadLine();
-            }
-        } while (flag);
-       
-
-    }
 }
 public class Bankkonto
 {
-    #region Klassenfelder
+
     public double kontostand { get; set; } = 0;
     private int _kontonummer;
     public int kontonummer
@@ -173,14 +122,7 @@ public class Bankkonto
         }
     }
 
-    #endregion
 
-    #region Konstruktoren
-
-    #endregion
-
-
-    #region Klassenmethoden
     public void Einzahlung(double einzahlbetrag)
     {
         kontostand += einzahlbetrag;
@@ -196,8 +138,5 @@ public class Bankkonto
         Console.WriteLine($"-----------------------\nIhr Kontostand:{kontostand}EUR");
         Console.ReadLine();
     }
-    #endregion
 
-    #region Statische Methoden
-    #endregion
 }
